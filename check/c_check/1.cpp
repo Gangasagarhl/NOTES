1c1a355f828aa728188dbc502aa2ab4eaf7ec3b0
Gangasagarhl <gangasagarhl1112@gmail.com>
2025-07-23 23:16:35 +0530

    recerent updates

--- check/c_check/1.cpp
+++ check/c_check/1.cpp
@@ -1,68 +1,78 @@
 #include<bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-int merge(vector<int> &b,  int low, int mid, int high){
 
-    vector<int> c;
-    int i=low, j=mid+1;
-    int cnt=0; 
+vector<int> BFS(vector<int> &adj[], int v){
 
-    while(i<=mid && j<= high)
+    vector<int> vis(v, 0);
+    vector<int> ans;
+    if(v==0) return ans;
+    queue<int> q;
+    q.push(0);
+    while(!q.empty()){
+
+        int node = q.front();
+        q.pop();
+        ans.push_back(node); 
+        for(auto x: adj[node]){
+            if(!vis[x]){
+                vis[x] =1;
+                q.push(x);            }
+        }
+        
+    }
+
+    return ans;
+
+}
+
+void DFS(int node, vector<int> adj[], vector<int> &ans, vector<int> &vis){
+
+    vis[node] =1; 
+    ans.push_back(node);
+    for(auto it: adj[node]){
+        if(!vis[it])
+         DFS(it, adj,ans, vis); 
+    }
+}
+
+
+vector<int> DFS_h(int v, vector<int>adj[])
+{
+    vector<int> vis(v,0); 
+    vector<int> ans;
+    for(int i=0;i<v;i++){
+    if(!vis[i]){
+        DFS(i,adj, ans, vis); 
+    } 
+}
+
+return ans;
+}
+
+
+int main(){
+
+    vector<vector<int>>matrix; 
+
+    vector<pair<int,int>>  adj[n]; 
+    for (int i=0;i<n;i++)
     {
-        if(b[i]<=b[j]){
-            c.push_back(b[i++]); 
+        for(int j=0;j<m;j++){
+
+            if (matirx[i][j]!=0)
+            {
+                adj[i].push_back({j,matrix[i][j]});
+            }
+
         }
-        else{
-            cnt+= (mid -i+1); 
-            c.push_back(b[j++]); 
-        }
+
     }
-
-    while(i<=mid){
-        c.push_back(b[i++]); 
-    }
-    while(j<=high){
-         c.push_back(b[j++]);
-    }
-
-    for(int k=0;k<c.size();k++){
-
-        b[low+k] = c[k];
-    }
-
-    return cnt; 
-
-
-}
-
-int  mergesort(vector<int> &b, int low, int high){
-   
-
-    if(low>=high) return 0;
-     int cnt= 0; 
-    int mid =(low+high)/2;
-    cnt+=mergesort(b, low, mid);
-    cnt+=mergesort(b, mid +1, high);
-    cnt+= merge(b,low, mid, high); 
-    return cnt;
     
-}
-
-int  main(){
-    vector<int> a={1,10,8,2,3,5,-1}, b=a, c=b;
-    sort(b.begin(), b.end()); 
-    cout<<"\n Sorted using library calls:  "; 
-    for(auto i: b){
-        cout<<i<<" "; 
-    }
-
-    int m = mergesort(c, 0, c.size()-1); 
-    cout<<"\n Sorted using custom merge sort :  "; 
-    for(auto i: c){
-        cout<<i<<" "; 
-    }
-    cout<<" the number of  inversion pirs we ave got is: "<<m;
+   for(auto  x: adj )
 
 
-    return 0;
+
+
+    return  0; 
 }
