405c69733a8f5a614d159c39eae021074838a840
Gangasagarhl <gangasagarhl1112@gmail.com>
2025-08-13 19:07:44 +0530

    statistics

--- check/c_check/1.cpp
+++ check/c_check/1.cpp
@@ -1,78 +1,133 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+private:
 
-vector<int> BFS(vector<int> &adj[], int v){
+    /* Function to perform DFS traversal 
+    while checking for safe nodes */
+    bool dfsCheck(int node, vector<int> adj[], 
+                  vector<int> &vis, 
+                  vector<int> &pathVis, 
+                  vector<int> &check) {
+                      
+        // Mark the node as visited
+		vis[node] = 1;
+		
+		// Add the node to current path
+		pathVis[node] = 1;
+		
+		// Mark the node as potentially unsafe
+		check[node] = 0;
+		
+		
+		// Traverse for adjacent nodes
+		for (auto it : adj[node]) {
+		    
+			// When the node is not visited
+			if (!vis[it]) {
+			    
+			    /* Perform DFS recursively and if 
+			    a cycle is found, return false */
+    			if (dfsCheck(it, adj, vis, pathVis, check)) {
+    			        
+    			        /* Return true since a 
+    			        cycle was detected */
+    					return true;
+    				}
 
-    vector<int> vis(v, 0);
-    vector<int> ans;
-    if(v==0) return ans;
-    queue<int> q;
-    q.push(0);
-    while(!q.empty()){
+			}
+			
+			/* Else if the node has been previously 
+			visited in the same path*/
+			else if (pathVis[it]) {
+				
+				/* Return true since a 
+		        cycle was detected */
+				return true;
+			}
+		}
+		
+		/* If the current node neither exist 
+		in a cycle nor points to a cycle, 
+		it can be marked as a safe node */
+		check[node] = 1;
+		
+		// Remove the node from the current path
+		pathVis[node] = 0;
+		
+		// Return false since no cycle was found
+		return false;
+	}
+    
+public:
 
-        int node = q.front();
-        q.pop();
-        ans.push_back(node); 
-        for(auto x: adj[node]){
-            if(!vis[x]){
-                vis[x] =1;
-                q.push(x);            }
-        }
-        
-    }
-
-    return ans;
-
-}
-
-void DFS(int node, vector<int> adj[], vector<int> &ans, vector<int> &vis){
-
-    vis[node] =1; 
-    ans.push_back(node);
-    for(auto it: adj[node]){
-        if(!vis[it])
-         DFS(it, adj,ans, vis); 
-    }
-}
-
-
-vector<int> DFS_h(int v, vector<int>adj[])
+	/* Function to get the
+	eventually safe nodes */
+	vector<int> eventualSafeNodes(int V, 
+	                vector<int> adj[]) 
 {
-    vector<int> vis(v,0); 
-    vector<int> ans;
-    for(int i=0;i<v;i++){
-    if(!vis[i]){
-        DFS(i,adj, ans, vis); 
-    } 
-}
+	    // Visited array
+	    vector<int> vis(V, false);
+	    
+	    // Path Visited array
+	    vector<int> pathVis(V, false);
+	    
+	    // To keep a check of safe nodes
+	    vector<int> check(V, false);
+	    
+	    /* Traverse the graph and 
+	    check for safe nodes */
+		for (int i = 0; i < V; i++) 
+        {
+			if (!vis[i]) 
+            {   // Start DFS traversal
+				dfsCheck(i, adj, vis, pathVis, check);
+			}
+		}
+		
+		// To store the result
+		vector<int> ans;
+		
+		// Add the safe nodes to the result
+		for (int i = 0; i < V; i++) 
+        {
+			if (check[i] == 1) 
+			    ans.push_back(i);
+		}
+		
+		// Return the result
+		return ans;
+	}
+};
 
-return ans;
-}
-
-
-int main(){
-
-    vector<vector<int>>matrix; 
-
-    vector<pair<int,int>>  adj[n]; 
-    for (int i=0;i<n;i++)
+int main() {
+    
+    int V = 7;
+    vector<int> adj[V] = {
+         {1,2},
+         {2,3},
+         {5},
+         {0},
+         {5},
+         {},
+         {}
+    };
+    
+    /* Creating an instance of 
+    Solution class */
+    Solution sol; 
+    
+    /* Function call to get the eventually 
+    safe nodes in the given graph */
+    vector<int> ans = sol.eventualSafeNodes(V, adj);
+    
+    // Output
+    cout << "The eventually safe nodes in the graph are:\n";
+    for(int i=0; i < ans.size(); i++) 
     {
-        for(int j=0;j<m;j++){
-
-            if (matirx[i][j]!=0)
-            {
-                adj[i].push_back({j,matrix[i][j]});
-            }
-
-        }
-
+        cout << ans[i] << " ";
     }
     
-   for(auto  x: adj )
-
-
-
-
-    return  0; 
+    return 0;
 }
--- check/c_check/2.cpp
+++ check/c_check/2.cpp
@@ -1,48 +1,126 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void  print(vector<vector<int>> &arr)
-{
-    for(auto i: arr){
-        for(auto j: i)
-            cout<< j<<"  ";
-        cout<<"\n"; 
+
+class Solution {
+private:
+
+    /* Function to perform DFS for storing the 
+    nodes in stack based on their finishing time */
+    void dfs(int node, vector<int> &vis, vector<int> adj[],
+             stack<int> &st) {
+
+        // Mark the node as visited
+        vis[node] = 1;
+        
+        // Traverse all its neighbors
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                // Recursively perform DFS is not visited already
+                dfs(it, vis, adj, st);
+            }
+        }
+        
+        // Push the node in stack
+        st.push(node);
     }
-}
-
-
-void sub(int ind ,vector<int> &arr, vector<vector<int>> &ans, vector<int> &nums){
-    if(ind==nums.size()){
-        ans.push_back(arr);
-        return;
-
-    }
-    arr.push_back(nums[ind]);
-    sub(ind+1, arr, ans, nums);
-    arr.pop_back(); 
-
-    for(int j= ind+1 ;j<nums.size();j++ ){
-        if(nums[j]!=nums[ind]){
-            sub(j, arr, ans, nums);
-            return;
+    
+    /* Helper function to perform DFS for finding
+    number of Strongly connected components */
+    void helperDFS(int node, vector<int> &vis, 
+                   vector<int> adjT[]) {
+                       
+        // Mark the node as visited
+        vis[node] = 1;
+        
+        // Traverse all its neighbors
+        for (auto it : adjT[node]) {
+            if (!vis[it]) {
+                // Recursively perform DFS if not already visited
+                helperDFS(it, vis, adjT);
+            }
         }
     }
+public:
 
-    sub(nums.size(), arr,ans, nums); 
-}
-
-
-
-int main(){
-    vector<int> arr={1, 2,2};
-    vector<vector<int>> ans; 
-    vector<int> ds; 
-    sub(0 ,ds, ans, arr);
-    
-    print(ans);
-
-
-    
-   
-    return 1;
+    /* Funtion call to find the number of 
+    strongly connected components */
+    int kosaraju(int V, vector<int> adj[]) {
+        
+        // Visited array
+        vector<int> vis(V, 0);
+        
+        // Stack data structure 
+        stack<int> st;
+        
+        /* Perform initial DFS to store the nodes 
+        in stack based on their finishing time */
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, vis, adj, st);
+            }
+        }
+        
+        // To store the revered graph
+        vector<int> adjT[V];
+        
+        /* Reverse all the edges of original 
+        graph to the reversed graph */
+        for (int i = 0; i < V; i++) {
+            
+            // Mark the node as unvisited
+            vis[i] = 0;
+            
+            // Add the reversed edge
+            for (auto it : adj[i]) {
+                adjT[it].push_back(i);
+            }
+        }
+        
+        /* To store the count of strongly 
+        connected components */
+        int count = 0;
+        
+        /* Start DFS call from every unvisited 
+        node based on their finishing time */
+        while (!st.empty()) {
+            // Get the node
+            int node = st.top();
+            st.pop();
+            
+            // If not visited already
+            if (!vis[node]) {
+                count += 1;
+                helperDFS(node, vis, adjT);
+            }
+        }
+        
+        // Return the result
+        return count;
+    }
 };
+
+int main() {
+
+    int n = 5;
+    int edges[5][2] = {
+        {1, 0}, {0, 2},
+        {2, 1}, {0, 3},
+        {3, 4}
+    };
+    
+    // Creating the adjacency list
+    vector<int> adj[n];
+    for (int i = 0; i < n; i++) {
+        adj[edges[i][0]].push_back(edges[i][1]);
+    }
+    
+    // Creating an instance of Solution class
+    Solution obj;
+    
+    /* Funtion call to find the number of 
+    strongly connected components */
+    int ans = obj.kosaraju(n, adj);
+    
+    cout << "The number of strongly connected components is: " << ans << endl;
+    return 0;
+}
